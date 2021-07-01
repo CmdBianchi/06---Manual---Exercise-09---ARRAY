d095101a49978bd52a8795cc4df77f5dda33c63c
marcus bianchi <marcusbianchi92@hotmail.com>
2021-07-01 18:24:03 -0300

    Adicionar arquivos de projeto.

---
+++ 06 - Manual - Exercise 09 - ARRAY/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace _06___Manual___Exercise_09___ARRAY {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("Digite o tamanho do vetor: "); 
+            int n = int.Parse(Console.ReadLine());
+            string[] fruits = new string[n]; 
+
+            for(int i =0; i < fruits.Length; i++) {
+                Console.WriteLine("Digite o nome #"+i);
+                fruits[i] = Console.ReadLine();
+            }
+
+            var result = Array.FindAll(fruits, fruits => fruits.Contains("A") 
+            || fruits.Contains("a") 
+            || fruits.Contains("B") 
+            || fruits.Contains("b"));
+           
+            int count = 0;
+            foreach (var fruit in result) {   
+                count += +1; 
+                Console.WriteLine(fruit);
+            }
+            Console.WriteLine(count);
+
+            Console.ReadKey();
+        }
+    }
+}
